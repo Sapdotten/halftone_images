6cc8290972f7bce036d26b8cd940a5ea5f2377ec
[Sapdotten] <[semenova.a.p.04@gmail.com]>
2023-09-14 22:00:53 +0400

    Add a gitignore

---
+++ include/halftone.h
@@ -0,0 +1,9 @@
+#ifndef HALFTONE_HALFTONE_H_
+#define HALFTONE_HALFTONE_H_
+namespace halftone {
+ template<typename T>
+class Image {
+
+};
+}//namespace halftone
+#endif
--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 int main()
 {
-	short a = 10;
-	a *= 1000000;
-	cout << typeid(a).name() << a  << endl;
+
 	return 0;
 }
